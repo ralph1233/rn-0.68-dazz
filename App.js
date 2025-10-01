2fd2def227ae98ed5d3751e643d1c1e8a35c0940
ralph1233 <ralphmerhy93@gmail.com>
2025-10-01 17:41:13 +0300

    vision camera not working

--- App.js
+++ App.js
@@ -1,45 +1,9 @@
-import {PureComponent} from 'react';
+import React, {PureComponent} from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
-import {Camera} from 'react-native-vision-camera';
-import {request, PERMISSIONS} from 'react-native-permissions';
 
 class App extends PureComponent {
-  componentDidMount() {
-    this.requestPermissions();
-  }
-
-  requestPermissions = async () => {
-    try {
-      const result = await request(PERMISSIONS.IOS.CAMERA);
-      console.log(result);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   render() {
-    console.log('Hello');
-    const device = Camera.getAvailableCameraDevices();
-
-    // ?.find(
-    //   device => device.position === 'back',
-    // );
-
-    console.log('Hello 2');
-
-    return null;
-
-    if (!device) {
-      return null;
-    }
-
-    console.log(device);
-
-    return (
-      <SafeAreaView style={styles.container}>
-        <Camera style={styles.camera} device={device} isActive={true} />
-      </SafeAreaView>
-    );
+    return <SafeAreaView style={styles.container}></SafeAreaView>;
   }
 }
 
@@ -47,10 +11,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-  },
-  camera: {
-    width: 300,
-    height: 300,
   },
 });
 
