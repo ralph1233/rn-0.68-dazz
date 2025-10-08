a124787339bac22b455b9c3e8b260d84ddd1cf90
ralph1233 <ralphmerhy93@gmail.com>
2025-10-08 13:16:46 +0300

    omar v2

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,9 @@
 /* eslint-disable prettier/prettier */
 import {Dimensions} from 'react-native';
 import Testing from './dazzfilters/Testing';
+import IR from './omarfiltersv2/IR';
+import OFMR from './omarfiltersv2/OFMR';
+import Hoga from './omarfiltersv2/Hoga';
 
 export const {width, height} = Dimensions.get('window');
 
@@ -12,6 +15,23 @@ export const imageFiltersWithCategory = [
         name: 'Testing',
         Element: Testing,
         icon: require('../assets/images/icons/vintage1.png'),
+      },
+      {
+        name: 'IR',
+        Element: IR,
+        icon: require('../assets/images/icons/b&w.png'),
+      },
+
+      {
+        name: 'OFMR',
+        Element: OFMR,
+        icon: require('../assets/images/icons/cassette.png'),
+      },
+
+      {
+        name: 'Hoga',
+        Element: Hoga,
+        icon: require('../assets/images/icons/sunset.png'),
       },
     ],
   },
